b271709f5349b983a0eb8bd19a33f4e93443211a
imraneamlal <imraneamlal789@gmail.com>
2023-06-16 11:13:44 -0700

    0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,11 +17,11 @@ int main(void)
 	}
 	if (n < 0)
 	{
-	printf("%d is negative\n", n)
+	printf("%d is negative\n", n);
 	}
 	if (n == 0)
 	{
-	printf("%d is zero\n", n)
+	printf("%d is zero\n", n);
 	}
 	return (0);
 }
